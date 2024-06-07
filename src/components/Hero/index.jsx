6af7544a02f2d86ad88a28ab66e567cac6f6ebba
rafaverde <rafaverde@msn.com>
2024-06-07 10:13:11 -0300

    Hero component adjusted

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,8 +5,9 @@ export function Hero() {
   return (
     <Container>
       <div>
+        <div id="hero_gradient"></div>
         <img src={droppingFoodImg} alt="Dropping flying food" />
-        <div>
+        <div id="hero_info">
           <h2>Sabores inigualáveis</h2>
           <h4>Sinta o cuidado do preparo com ingredientes selecionados.</h4>
         </div>
--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -4,31 +4,47 @@ export const Container = styled.div`
   width: 100%;
   padding: 3rem;
 
-  position: relative;
-  overflow: auto;
-
   > div {
-    background: ${({ theme }) => theme.GRADIENTS.GRADIENT_200};
-
     display: flex;
     align-items: center;
-    gap: 2rem;
+    gap: 0;
 
-    padding-right: 2rem;
+    position: relative;
+
+    /* padding: 2rem; */
 
     border-radius: 1rem;
 
     > img {
-      max-width: 40%;
+      max-width: 60%;
+    }
+
+    #hero_info {
+      padding: 30px 10px 30px 0;
     }
 
     h2 {
-      font-size: 2rem;
+      font-size: 1.4rem;
       line-height: 1;
     }
 
     h4 {
+      font-size: 0.9rem;
       font-weight: 300;
     }
+  }
+
+  #hero_gradient {
+    width: 90%;
+    height: 70%;
+
+    right: 0;
+    /* bottom: 10px; */
+
+    position: absolute;
+    z-index: -1;
+
+    border-radius: 0.7rem;
+    background: ${({ theme }) => theme.GRADIENTS.GRADIENT_200};
   }
 `
