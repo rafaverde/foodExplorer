24a76429698b99370fc1b2e447daf7a38265a705
rafaverde <rafaverde@msn.com>
2024-06-15 08:15:34 -0300

    Everything related to styles and interface has been centralized in UIContext.

--- src/hooks/ui.jsx
+++ src/hooks/ui.jsx
@@ -2,25 +2,26 @@ import { createContext, useContext, useState } from "react"
 
 import { ThemeProvider } from "styled-components"
 import { darkTheme, lightTheme } from "../styles/theme"
+import GlobalStyles from "../styles/global"
 
 export const UIContext = createContext({})
 
 function UIProvider({ children }) {
-  //Theme Mode
+  //Theme Mode manipulation
   const [isDarkTheme, setIsDarkTheme] = useState(false)
 
   function toggleThemeMode() {
     setIsDarkTheme((prevState) => !prevState)
   }
 
-  //Side Menu functionalities
+  //Side Menu manipulation
   const [menuIsOpen, setMenuIsOpen] = useState(false)
 
   function toggleSideMenu() {
     setMenuIsOpen((prevState) => !prevState)
   }
 
-  //Returning provider
+  //Returning ui and theme providers
   return (
     <UIContext.Provider
       value={{
@@ -31,6 +32,7 @@ function UIProvider({ children }) {
       }}
     >
       <ThemeProvider theme={!isDarkTheme ? darkTheme : lightTheme}>
+        <GlobalStyles />
         {children}
       </ThemeProvider>
     </UIContext.Provider>
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 import { UIProvider } from "./hooks/ui"
-import GlobalStyles from "./styles/global"
 
 import { Routes } from "./routes"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UIProvider>
-      <GlobalStyles />
       <Routes />
     </UIProvider>
   </React.StrictMode>
