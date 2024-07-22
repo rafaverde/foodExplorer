613e525f3e09abe58fbddadfac6ca3bdf69d300e
rafaverde <rafaverde@msn.com>
2024-07-22 14:25:41 -0300

    Minor adjustment.

--- src/routes/admin.routes.jsx
+++ src/routes/admin.routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 
 import { Home } from "../pages/Home"
 import { PlateDetail } from "../pages/PlateDetail"
