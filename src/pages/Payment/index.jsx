1b4091e160277c1c861f719f94a6dfdaa6b11df0
rafaverde <rafaverde@msn.com>
2024-07-25 12:54:47 -0300

    Created payment simulation interface. Interaction between review and payment.

--- src/pages/Payment/index.jsx
+++ src/pages/Payment/index.jsx
@@ -8,9 +8,19 @@ import {
   OrderPayment,
   OrderReview,
   OrderWrapper,
+  Tabs,
   Total,
+  PaymentDialog,
+  TabOutput,
+  PixTab,
+  CardTab,
 } from "./styles"
-import { CaretCircleLeft, SmileySad } from "@phosphor-icons/react"
+import {
+  CaretCircleLeft,
+  CreditCard,
+  PixLogo,
+  SmileySad,
+} from "@phosphor-icons/react"
 
 import { Footer } from "../../components/Footer"
 import { Header } from "../../components/Header"
@@ -22,16 +32,21 @@ import { Button } from "../../components/Button"
 import { useUI } from "../../hooks/ui"
 import { useAuth } from "../../hooks/auth"
 import { useState } from "react"
+import { Input } from "../../components/Input"
 
 export function Payment() {
   const { orderItems } = useUI()
   const { user } = useAuth()
 
-  //Interface Controle
+  //Interface Control
   const [toggleReview, setToggleReview] = useState(true)
-
   function handleToggleReview() {
     setToggleReview((prevState) => !prevState)
+  }
+
+  const [toggleOutput, setToggleOutput] = useState("pix")
+  function handleToggleOutput(value) {
+    setToggleOutput(value)
   }
 
   //Order Total
@@ -109,11 +124,79 @@ export function Payment() {
               <Message>
                 <SmileySad size={100} />
                 <span>Nenhum item no seu pedido!</span>
+                <ButtonText
+                  title="Navegue em nosso Cardápio!"
+                  onClick={() => navigate("/")}
+                />
               </Message>
             ) : null}
           </OrderReview>
           <OrderPayment className={toggleReview ? "inactive" : "active"}>
             <CategorySection title="Pagamento" />
+            <PaymentDialog>
+              <Tabs
+                className="left-tab"
+                onClick={() => handleToggleOutput("pix")}
+              >
+                <PixLogo size={26} />
+                <p>PIX</p>
+              </Tabs>
+              <Tabs
+                className="right-tab"
+                onClick={() => handleToggleOutput("card")}
+              >
+                <CreditCard size={26} />
+                <p>Cartão</p>
+              </Tabs>
+              <TabOutput>
+                <PixTab
+                  className={toggleOutput === "pix" ? "active" : "inactive"}
+                >
+                  <img
+                    src="https://api.qrserver.com/v1/create-qr-code/?color=000000&amp;bgcolor=FFFFFF&amp;data=https%3A%2F%2Fgithub.com%2Frafaverde&amp;qzone=1&amp;margin=0&amp;ecc=L"
+                    alt="qr code"
+                  />
+                </PixTab>
+                <CardTab
+                  className={toggleOutput === "card" ? "active" : "inactive"}
+                >
+                  <h2>Dados do Cartão</h2>
+                  <form>
+                    <div className="input-wrapper">
+                      <label htmlFor="cardNumber">Número do Cartão</label>
+                      <Input
+                        type="text"
+                        id="cardNumber"
+                        placeholder="0000 0000 0000 0000"
+                        readOnly
+                      />
+                    </div>
+                    <div className="col-2">
+                      <div className="input-wrapper">
+                        <label htmlFor="cardDate">Vencimento</label>
+                        <Input
+                          type="text"
+                          id="cardDate"
+                          placeholder="00/00"
+                          readOnly
+                        />
+                      </div>
+                      <div className="input-wrapper">
+                        <label htmlFor="cardCVC">CVC</label>
+                        <Input
+                          type="text"
+                          id="cardCVC"
+                          placeholder="000"
+                          readOnly
+                        />
+                      </div>
+                    </div>
+                  </form>
+                </CardTab>
+
+                <Button title="Simular pagamento" />
+              </TabOutput>
+            </PaymentDialog>
             <Button
               title="Revisar Pedido"
               icon={CaretCircleLeft}
--- src/pages/Payment/styles.js
+++ src/pages/Payment/styles.js
@@ -12,7 +12,7 @@ export const Container = styled.div`
 export const Content = styled.div`
   width: 100%;
   height: calc(100vh - (114px + 77px + 30px));
-  padding: 30px 3rem;
+  padding: 30px 3rem 0;
 
   grid-area: content;
   overflow-y: auto;
@@ -39,6 +39,7 @@ export const OrderWrapper = styled.div`
   overflow-y: auto;
 `
 
+//Order Review
 export const OrderReview = styled.div`
   min-width: calc(100vw - 6rem);
   transition: all 1s;
@@ -93,11 +94,122 @@ export const Message = styled.div`
   }
 `
 
+//Order Payment
 export const OrderPayment = styled.div`
   min-width: calc(100vw - 6rem);
   transition: all 1s;
 
   &.active {
     transform: translateX(calc(-100vw + 3rem));
+    height: auto;
+  }
+`
+
+export const PaymentDialog = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+`
+
+export const Tabs = styled.div`
+  width: 50%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+
+  cursor: pointer;
+
+  border: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_300};
+  transition: all 500ms;
+
+  &.left-tab {
+    border-radius: 0.7rem 0 0 0;
+  }
+  &.right-tab {
+    border-radius: 0 0.7rem 0 0;
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+  }
+
+  > p {
+    font-weight: bold;
+    font-size: 1.1rem;
+  }
+`
+
+export const TabOutput = styled.div`
+  min-width: 100%;
+  padding: 30px 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 0 0 0.7rem 0.7rem;
+  border: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_300};
+
+  > button {
+    margin-top: 20px;
+  }
+`
+
+export const PixTab = styled.div`
+  text-align: center;
+
+  > img {
+    width: 50%;
+  }
+
+  &.active {
+    opacity: 1;
+    transition: all 1s;
+  }
+
+  &.inactive {
+    opacity: 0;
+    width: 0;
+  }
+`
+
+export const CardTab = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  &.inactive > h2 + form {
+    display: none;
+  }
+
+  > form {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    font-size: 1.1rem;
+  }
+
+  .input-wrapper div {
+    margin-top: 8px;
+  }
+
+  > form .col-2 {
+    display: flex;
+    gap: 12px;
+  }
+
+  &.active {
+    opacity: 1;
+    transition: opacity 1s;
+  }
+
+  &.inactive {
+    opacity: 0;
+    width: 0;
+    height: 0;
   }
 `
