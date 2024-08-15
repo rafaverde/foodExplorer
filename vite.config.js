0df484686c006e8a3c291b3157a7d83dbd8d2944
rafaverde <rafaverde@msn.com>
2024-08-15 16:01:11 -0300

    Vite vrd config removed.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://foodexplorer.vrd.ppg.br/",
+  // base: "https://foodexplorer.vrd.ppg.br/",
   plugins: [react()],
 })
